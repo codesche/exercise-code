b4caf7abf241d5aeeb52adfb0bf21fc59597c3d4
codesche <codesche@gmail.com>
2025-02-25 16:03:54 +0900

    [polymorphism] compiletime package > Main Class

---
+++ polymorphism/src/main/java/complietime/Main.java
@@ -0,0 +1,11 @@
+package complietime;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+
+        triangle.draw();
+        triangle.draw("red");
+        triangle.draw(10, "blue");
+    }
+}
