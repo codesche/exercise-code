6fbfec760ad9e239bc54c11134751e1b20fcdd88
codesche <codesche@gmail.com>
2025-03-02 00:17:38 +0900

    리스코프 치환원칙 실습: bad > Member

---
+++ LiskovSubstitutionPrinciple/src/main/java/bad/Member.java
@@ -0,0 +1,13 @@
+package bad;
+
+public abstract class Member {
+    private final String name;
+
+    public Member(String name) {
+        this.name = name;
+    }
+
+    public abstract void joinTournament();
+    public abstract void organizeTournament();
+
+}
